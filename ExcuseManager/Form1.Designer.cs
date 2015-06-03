2e8defaa612a5ed88d5d262cd646b0c00b126cfc
mamaker <madhuvasudevan@hotmail.com>
2015-06-02 23:48:43 -0500

    Done!

--- ExcuseManager/Form1.Designer.cs
+++ ExcuseManager/Form1.Designer.cs
@@ -28,12 +28,181 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.description = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.results = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.folder = new System.Windows.Forms.Button();
+            this.save = new System.Windows.Forms.Button();
+            this.open = new System.Windows.Forms.Button();
+            this.randomExcuse = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.lastUsed = new System.Windows.Forms.DateTimePicker();
+            this.fileDate = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Excuse";
+            // 
+            // description
+            // 
+            this.description.Location = new System.Drawing.Point(101, 10);
+            this.description.Name = "description";
+            this.description.Size = new System.Drawing.Size(261, 20);
+            this.description.TabIndex = 1;
+            this.description.TextChanged += new System.EventHandler(this.description_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 37);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Results";
+            // 
+            // results
+            // 
+            this.results.Location = new System.Drawing.Point(101, 34);
+            this.results.Name = "results";
+            this.results.Size = new System.Drawing.Size(261, 20);
+            this.results.TabIndex = 3;
+            this.results.TextChanged += new System.EventHandler(this.results_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 67);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Last Used";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 93);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(49, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "File Date";
+            // 
+            // folder
+            // 
+            this.folder.Location = new System.Drawing.Point(12, 123);
+            this.folder.Name = "folder";
+            this.folder.Size = new System.Drawing.Size(75, 23);
+            this.folder.TabIndex = 6;
+            this.folder.Text = "Folder";
+            this.folder.UseVisualStyleBackColor = true;
+            this.folder.Click += new System.EventHandler(this.folder_Click);
+            // 
+            // save
+            // 
+            this.save.Enabled = false;
+            this.save.Location = new System.Drawing.Point(101, 123);
+            this.save.Name = "save";
+            this.save.Size = new System.Drawing.Size(75, 23);
+            this.save.TabIndex = 7;
+            this.save.Text = "Save";
+            this.save.UseVisualStyleBackColor = true;
+            this.save.Click += new System.EventHandler(this.save_Click);
+            // 
+            // open
+            // 
+            this.open.Enabled = false;
+            this.open.Location = new System.Drawing.Point(194, 123);
+            this.open.Name = "open";
+            this.open.Size = new System.Drawing.Size(75, 23);
+            this.open.TabIndex = 8;
+            this.open.Text = "Open";
+            this.open.UseVisualStyleBackColor = true;
+            this.open.Click += new System.EventHandler(this.open_Click);
+            // 
+            // randomExcuse
+            // 
+            this.randomExcuse.Enabled = false;
+            this.randomExcuse.Location = new System.Drawing.Point(287, 123);
+            this.randomExcuse.Name = "randomExcuse";
+            this.randomExcuse.Size = new System.Drawing.Size(75, 23);
+            this.randomExcuse.TabIndex = 9;
+            this.randomExcuse.Text = "Random";
+            this.randomExcuse.UseVisualStyleBackColor = true;
+            this.randomExcuse.Click += new System.EventHandler(this.randomExcuse_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // lastUsed
+            // 
+            this.lastUsed.Location = new System.Drawing.Point(101, 60);
+            this.lastUsed.Name = "lastUsed";
+            this.lastUsed.Size = new System.Drawing.Size(200, 20);
+            this.lastUsed.TabIndex = 10;
+            this.lastUsed.ValueChanged += new System.EventHandler(this.lastUsed_ValueChanged);
+            // 
+            // fileDate
+            // 
+            this.fileDate.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.fileDate.Location = new System.Drawing.Point(101, 83);
+            this.fileDate.Name = "fileDate";
+            this.fileDate.Size = new System.Drawing.Size(200, 23);
+            this.fileDate.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(559, 193);
+            this.Controls.Add(this.fileDate);
+            this.Controls.Add(this.lastUsed);
+            this.Controls.Add(this.randomExcuse);
+            this.Controls.Add(this.open);
+            this.Controls.Add(this.save);
+            this.Controls.Add(this.folder);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.results);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.description);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Excuse Manager";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox description;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox results;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button folder;
+        private System.Windows.Forms.Button save;
+        private System.Windows.Forms.Button open;
+        private System.Windows.Forms.Button randomExcuse;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.DateTimePicker lastUsed;
+        private System.Windows.Forms.Label fileDate;
     }
 }
 
